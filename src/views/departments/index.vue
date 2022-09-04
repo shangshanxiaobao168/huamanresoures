48792da5691ad57d5aec47cf1be3885577eb464b
xiaobao168 <fyf168_win@163.com>
2022-09-04 15:38:33 +0800

    将树形内容单独封装成组件

--- src/views/departments/index.vue
+++ src/views/departments/index.vue
@@ -1,13 +1,41 @@
 <template>
-  <div>组织架构页面</div>
+  <div class="dashboard-container">
+    <div class="app-container">
+      <el-card class="box-card">
+        <!-- 头部 -->
+        <treeTools :isRoot="true" :treeNode="company" />
+        <!-- 树形 -->
+        <el-tree :data="treeData" default-expand-all :props="defaultProps">
+          <treeTools :treeNode="{ name: '总裁办', manager: '周杰伦' }" />
+        </el-tree>
+      </el-card>
+    </div>
+  </div>
 </template>
 
 <script>
+import treeTools from './components/tree-tools.vue'
 export default {
   data() {
-    return {}
+    return {
+      treeData: [
+        { name: '总裁办', children: [{ name: '董事会' }] },
+        { name: '行政部' },
+        { name: '人事部' },
+      ],
+      defaultProps: {
+        label: 'name', //将data中哪个数据名显示到树形页面中，
+        // children: 'child',//树形默认查找子节点通过children
+      },
+      company: {
+        name: '传智教育',
+        manager: '负责人',
+      },
+    }
   },
-
+  components: {
+    treeTools,
+  },
   created() {},
 
   methods: {},
