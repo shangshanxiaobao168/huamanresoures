6d3cee12f679c508189f7f079d34922554f4771e
xiaobao168 <fyf168_win@163.com>
2022-09-15 22:33:32 +0800

    完成公司设置的静态页面

--- src/views/setting/index.vue
+++ src/views/setting/index.vue
@@ -1,27 +1,56 @@
 <template>
   <div>
-   公司设置页面
+    <el-tabs v-model="activeName" @tab-click="handleClick">
+      <el-tab-pane label="角色管理" name="first"
+        ><el-button type="primary">角色管理</el-button></el-tab-pane
+      >
+      <el-tab-pane label="公司信息" name="second">
+        <el-table :data="tableData" style="width: 100%">
+          <el-table-column prop="date" label="日期" width="180">
+          </el-table-column>
+          <el-table-column prop="name" label="姓名" width="180">
+          </el-table-column>
+          <el-table-column prop="address" label="地址"> </el-table-column>
+        </el-table>
+      </el-tab-pane>
+    </el-tabs>
   </div>
 </template>
 
 <script>
 export default {
-  data () {
+  data() {
     return {
-
+      activeName: 'first',
+      tableData: [
+        {
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄',
+        },
+        {
+          date: '2016-05-04',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1517 弄',
+        },
+        {
+          date: '2016-05-01',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1519 弄',
+        },
+        {
+          date: '2016-05-03',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1516 弄',
+        },
+      ],
     }
   },
 
-  created () {
+  created() {},
 
-  },
-
-  methods: {
-
-  }
+  methods: {},
 }
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
